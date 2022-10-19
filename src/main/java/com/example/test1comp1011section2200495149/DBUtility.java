d6ac2da4686648f35666f9fdb0e55e763286813e
Joshua Lebreton <200495149@student.georgianc.on.ca>
2022-10-19 12:35:50 -0400

    Question 3a complete

--- src/main/java/com/example/test1comp1011section2200495149/DBUtility.java
+++ src/main/java/com/example/test1comp1011section2200495149/DBUtility.java
@@ -11,9 +11,9 @@ public class DBUtility {
     private static String pw = "student";
     private static String connectUrl = "jdbc:mysql://127.0.0.1:3306/javatest";
 
-    public static ArrayList<NetflixShow> getWorkoutDataFromDB()
+    public static ArrayList<NetflixShow> getNetflixShowsFromDB()
     {
-        ArrayList<NetflixShow> workouts = new ArrayList<>();
+        ArrayList<NetflixShow> netflixShows = new ArrayList<>();
 
         String sql =    "SELECT showId, type, title, rating, director, cast \n" +
                         "FROM netflix;";
@@ -36,13 +36,44 @@ public class DBUtility {
                 String director = dbResults.getString("director");
                 String cast = dbResults.getString("cast");
                 NetflixShow newShow = new NetflixShow(showId,type, title, rating, director, cast);
-                workouts.add(newShow);
+                netflixShows.add(newShow);
 
             }
         } catch (Exception e)
         {
             e.printStackTrace();
         }
-        return workouts;
+        return netflixShows;
+    }
+
+    public static ArrayList<String> getRatingsFromDB()
+    {
+        ArrayList<String> ratings = new ArrayList<>();
+
+        String sql =    "SELECT rating "    +
+                        "FROM netflix "     +
+                        "GROUP BY rating "  +
+                        "ORDER BY rating;";
+
+        try(
+                Connection conn = DriverManager.getConnection(connectUrl,user,pw);
+
+                Statement statement = conn.createStatement();
+
+                ResultSet dbResults = statement.executeQuery(sql);
+        )
+        {
+
+            while (dbResults.next())
+            {
+                String rating = dbResults.getString("rating");
+                ratings.add(rating);
+
+            }
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return ratings;
     }
 }
---
+++ src/main/java/com/example/test1comp1011section2200495149/NetflixShowTableViewController.java
@@ -0,0 +1,26 @@
+package com.example.test1comp1011section2200495149;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+
+public class NetflixShowTableViewController {
+
+    @FXML
+    private TableColumn<?, ?> castColumn;
+
+    @FXML
+    private TableColumn<?, ?> directorColumn;
+
+    @FXML
+    private TableColumn<?, ?> ratingColumn;
+
+    @FXML
+    private TableColumn<?, ?> showIdColumn;
+
+    @FXML
+    private TableColumn<?, ?> titleColumn;
+
+    @FXML
+    private TableColumn<?, ?> typeColumn;
+
+}
