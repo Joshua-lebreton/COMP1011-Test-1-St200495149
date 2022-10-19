26949f9a7d9ebd564d957728b9d49f0a43ec2fa2
Joshua Lebreton <200495149@student.georgianc.on.ca>
2022-10-19 11:21:32 -0400

    First commit

---
+++ src/main/java/com/example/test1comp1011section2200495149/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.test1comp1011section2200495149;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.test1comp1011section2200495149 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.test1comp1011section2200495149 to javafx.fxml;
+    exports com.example.test1comp1011section2200495149;
+}
