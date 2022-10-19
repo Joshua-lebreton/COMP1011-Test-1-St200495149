6d30828828b75ee0ed5775cc377a5460efda600e
Joshua Lebreton <200495149@student.georgianc.on.ca>
2022-10-19 12:49:11 -0400

    Question 3b complete

--- src/main/java/com/example/test1comp1011section2200495149/NetflixShowTableViewController.java
+++ src/main/java/com/example/test1comp1011section2200495149/NetflixShowTableViewController.java
@@ -1,26 +1,56 @@
 package com.example.test1comp1011section2200495149;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class NetflixShowTableViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
-    @FXML
-    private TableColumn<?, ?> castColumn;
-
-    @FXML
-    private TableColumn<?, ?> directorColumn;
-
-    @FXML
-    private TableColumn<?, ?> ratingColumn;
+public class NetflixShowTableViewController implements Initializable {
 
     @FXML
-    private TableColumn<?, ?> showIdColumn;
+    private TableColumn<NetflixShow, String> castColumn;
 
     @FXML
-    private TableColumn<?, ?> titleColumn;
+    private TableColumn<NetflixShow, String> directorColumn;
 
     @FXML
-    private TableColumn<?, ?> typeColumn;
+    private TableColumn<NetflixShow, String> ratingColumn;
 
+    @FXML
+    private TableColumn<NetflixShow, Integer> showIdColumn;
+
+    @FXML
+    private TableColumn<NetflixShow, String> titleColumn;
+
+    @FXML
+    private TableColumn<NetflixShow, String> typeColumn;
+
+    @FXML
+    private TableView<NetflixShow> showTable;
+
+    @FXML
+    private CheckBox movieCheckBox;
+
+    @FXML
+    private CheckBox tvShowCheckBox;
+
+    @FXML
+    private Label numberOfShowsLabel;
+
+    @FXML
+    private ComboBox<String> ratingComboBox;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        castColumn.setCellValueFactory(new PropertyValueFactory<>("cast"));
+        directorColumn.setCellValueFactory(new PropertyValueFactory<>("director"));
+        ratingColumn.setCellValueFactory(new PropertyValueFactory<>("rating"));
+        showIdColumn.setCellValueFactory(new PropertyValueFactory<>("showId"));
+        titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
+        typeColumn.setCellValueFactory(new PropertyValueFactory<>("type"));
+        showTable.getItems().addAll(DBUtility.getNetflixShowsFromDB());
+    }
 }
