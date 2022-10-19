ab82430c2aa6efd8006d88fcc15a7aef7184fa7f
Joshua Lebreton <200495149@student.georgianc.on.ca>
2022-10-19 11:35:48 -0400

    1.A complete

--- src/main/java/com/example/test1comp1011section2200495149/NetfilxShow.java
+++ src/main/java/com/example/test1comp1011section2200495149/NetfilxShow.java
@@ -4,5 +4,65 @@ public class NetfilxShow {
     private int showId;
     private String type, title, rating, director, cast;
 
+    public NetfilxShow(int showId, String type, String title, String rating, String director, String cast) {
+        setShowId(showId);
+        setType(type);
+        setTitle(title);
+        setRating(rating);
+        setDirector(director);
+        setCast(cast);
+        }
 
+    public int getShowId() {
+        return showId;
+    }
+
+    public void setShowId(int showId) {
+        if(showId > 0){
+            this.showId = showId;
+        }else{
+            throw new IllegalArgumentException("showId must be greater then 0");
+        }
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getCast() {
+        return cast;
+    }
+
+    public void setCast(String cast) {
+        this.cast = cast;
+    }
 }
